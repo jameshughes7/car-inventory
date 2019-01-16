89734a57901b5cb2bdef852087c3854567a0331e
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-01-16 15:14:58 +0000

    updated to es6 syntax and type declaration library added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-const express = require('express');
+import express from "express";
 const app = express();
-const port = 8080;
+const port = 8080; // default port to listen
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
-})
+});
 
 app.listen(port, () => {
+    // tslint:disable-next-line:no-console
     console.log(`App server starting at http://localhost:${port}`);
 });
