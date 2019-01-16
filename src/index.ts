7749fc5f4c35258282ce4cd8671d68fb8fe18b8f
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-01-16 15:38:08 +0000

    views created and ejs added as view engine

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import path from 'path';
 const app = express();
 const port = 8080; // default port to listen
 
+
+//Configure express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+//Define route handler for the default homepage
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+
+//Start the express server
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`App server starting at http://localhost:${port}`);
