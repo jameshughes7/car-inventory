17996bca2c35a357d96cb9dab4667e861ac0d568
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-01-16 15:51:53 +0000

    added asset build script for Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import express from "express";
-import path from 'path';
+import path from "path";
 const app = express();
 const port = 8080; // default port to listen
 
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
 
-//Configure express to use EJS
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.render( "index" );
+} );
 
-
-//Define route handler for the default homepage
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-
-//Start the express server
-app.listen(port, () => {
+// start the express server
+app.listen( port, () => {
     // tslint:disable-next-line:no-console
-    console.log(`App server starting at http://localhost:${port}`);
-});
+    console.log( `server started at http://localhost:${ port }` );
+} );
